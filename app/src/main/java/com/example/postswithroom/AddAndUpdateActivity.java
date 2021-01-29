7aa304ae2a2166c372bedc301028be21fd0bb31d
rahma999 <rahmafathy@gmail.com>
2021-01-29 02:42:13 +0200

    Shared prefrences

--- app/src/main/java/com/example/postswithroom/AddAndUpdateActivity.java
+++ app/src/main/java/com/example/postswithroom/AddAndUpdateActivity.java
@@ -1,14 +1,16 @@
 package com.example.postswithroom;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.muddzdev.styleabletoast.StyleableToast;
 
@@ -21,82 +23,106 @@ import Data_Base.Note_Entity;
 public class AddAndUpdateActivity extends AppCompatActivity {
     EditText add_title;
     EditText add_description;
+    Intent data;
+    String button;
+    Toolbar toolbar;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-
+        sp = getSharedPreferences("notes", Context.MODE_PRIVATE);
+        editor = sp.edit();
         initView();
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        button = data.getExtras().getString("button");
+        editor.putString("button", button);
+        editor.putString("title", add_title.getText().toString());
+        editor.putString("description", add_description.getText().toString());
+       editor.putInt("id", data.getExtras().getInt("id"));
+        editor.commit();
+        boolean save = sp.getBoolean("save", true);
+        if (save == true) {
+            StyleableToast.makeText(this, "Your note is saved", Toast.LENGTH_SHORT, R.style.mytoast).show();
+        } else {
+            StyleableToast.makeText(this, "Your note saved in draft", Toast.LENGTH_SHORT, R.style.mytoast).show();
+        }
+    }
+
+
     public void initView() {
+        toolbar = findViewById(R.id.includeToolbar);
         add_title = findViewById(R.id.add_title);
         add_description = findViewById(R.id.add_description);
         Button btn_save = findViewById(R.id.btn_save);
-
-        final Intent data = getIntent();
-        String title = data.getExtras().getString("note_title");
-        String description = data.getExtras().getString("note_describtion");
-        String button = data.getExtras().getString("button");
+        setSupportActionBar(toolbar);
+        data = getIntent();
+        button = data.getExtras().getString("button");
         final int id = data.getExtras().getInt("id");
         btn_save.setText(button);
-        add_title.setText(title);
-        add_description.setText(description);
+        add_title.setText(data.getExtras().getString("note_title"));
+        add_description.setText(data.getExtras().getString("note_describtion"));
 
 
         if (button.equalsIgnoreCase("save")) {
+            editor.putBoolean("save", false);
             btn_save.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    editor.putBoolean("save", true);
                     //date
                     Calendar calendar = Calendar.getInstance();
                     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, dd-MMM-yyyy hh:mm:ss a");
                     String date = simpleDateFormat.format(calendar.getTime());
 //                    String date = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
 
-                    String title = add_title.getText().toString();
-                    String description = add_description.getText().toString();
                     //validation to make sure the note is not null
-                    if (description.isEmpty()) {
+                    if (add_description.getText().toString().isEmpty()) {
                         // Toast.makeText(AddAndUpdateActivity.this,"Please Enter your note",Toast.LENGTH_SHORT).show();
                         StyleableToast.makeText(AddAndUpdateActivity.this, "Make sure you enter your note", Toast.LENGTH_LONG, R.style.mytoast).show();
                         add_description.requestFocus();
                         add_description.setError("Enter your note");
                     } else {
-                        Intent intent=new Intent();
-                        Note_Entity note = new Note_Entity(title, description, date);
+                        Intent intent = new Intent();
+                        Note_Entity note = new Note_Entity(add_title.getText().toString(), add_description.getText().toString(), date);
                         NoteDataBase.getInstance(AddAndUpdateActivity.this).noteDao().addPost(note);
-                   // startActivity(new Intent(AddAndUpdateActivity.this, MainActivity.class));
+//                   startActivity(new Intent(AddAndUpdateActivity.this, MainActivity.class));
+                        setResult(RESULT_OK, intent);
+                        finish();
 
-                       setResult(RESULT_OK,intent);
-                       finish();
                     }
                 }
             });
+
         } else {
+            editor.putBoolean("save", false);
             btn_save.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    editor.putBoolean("save", true);
                     Calendar calendar = Calendar.getInstance();
                     //date by format
                     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE,dd-MMM-yyyy hh:mm:ss a");
                     String date = simpleDateFormat.format(calendar.getTime());
 //                    String date = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
-                    String title = add_title.getText().toString();
-                    String description = add_description.getText().toString();
                     //validation
-                    if (description.isEmpty()) {
+                    if (add_description.getText().toString().isEmpty()) {
                         add_description.setError("Enter your new note");
                         add_description.requestFocus();
                         StyleableToast.makeText(AddAndUpdateActivity.this, "Make sure you enter your note after edit it", Toast.LENGTH_LONG, R.style.mytoast).show();
                         //  Toast.makeText(AddAndUpdateActivity.this,"Please Enter your note after your Edit",Toast.LENGTH_SHORT).show();
                     } else {
-Intent intent=new Intent();
-                        NoteDataBase.getInstance(AddAndUpdateActivity.this).noteDao().updatePost(id, title, description, date);
-                        //startActivity(new Intent(AddAndUpdateActivity.this, MainActivity.class));
-                        setResult(RESULT_OK,intent);
+
+                        NoteDataBase.getInstance(AddAndUpdateActivity.this).noteDao().updatePost(id, add_title.getText().toString(), add_description.getText().toString(), date);
+                        Intent intent = new Intent();
+                        setResult(RESULT_OK, intent);
                         finish();
                     }
                 }
@@ -104,6 +130,7 @@ Intent intent=new Intent();
 
 
         }
+
     }
 //ممكن اعملها بميثود واستدعيها فوق
 //    public boolean validationNote(EditText description){
@@ -114,6 +141,32 @@ Intent intent=new Intent();
 //        }else {
 //            return true;
 //        }}
+
+
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        String title_re =sp.getString("title"," hg");
+//        String description_re = sp.getString("description","jh");
+//        add_title.setText(title_re);
+//        add_description.setText(description_re);
+//        Toast.makeText(this,"ghfh",Toast.LENGTH_SHORT).show();
+//    }
+
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        sp= PreferenceManager.getDefaultSharedPreferences(this);
+//        editor=sp.edit();
+//        editor.putString("title",add_title.getText().toString());
+//        editor.putString("description",add_description.getText().toString());
+//        editor.apply();
+//        String title_re =sp.getString("title"," hg");
+//       String description_re = sp.getString("description","jh");
+//       add_title.setText(title_re);
+//       add_description.setText(description_re);
+//Toast.makeText(this,"ghfh",Toast.LENGTH_SHORT).show();
+//    }
 }
 
 
--- app/src/main/java/com/example/postswithroom/MainActivity.java
+++ app/src/main/java/com/example/postswithroom/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.postswithroom;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.muddzdev.styleabletoast.StyleableToast;
 
 import java.util.List;
 
@@ -24,21 +30,57 @@ public class MainActivity extends AppCompatActivity {
     List<Note_Entity> list;
     Recycler_Adapter adapter;
     RecyclerView main_recycler;
-    Note_Entity note_entity;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.includeToolbar);
+        setSupportActionBar(toolbar);
         initView();
-putRecycle();
+        putRecycle();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.archive:
+                SharedPreferences sh = getSharedPreferences("notes", MODE_PRIVATE);
+                String title_re = sh.getString("title", "");
+                String description_re = sh.getString("description", "");
+                String button = sh.getString("button", "");
+                boolean save = sh.getBoolean("save", true);
+                int id=sh.getInt("id",0);
+
+                if (save == true) {
+                    StyleableToast.makeText(this, "Your draft is empty", Toast.LENGTH_SHORT, R.style.mytoast).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, AddAndUpdateActivity.class);
+                    intent.putExtra("note_title", title_re);
+                    intent.putExtra("note_describtion", description_re);
+                    intent.putExtra("button", button);
+                    intent.putExtra("id", id);
+                    startActivityForResult(intent, 1);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     public void initView() {
 
         FloatingActionButton fab_add = findViewById(R.id.fab_add);
         FloatingActionButton fab_delete = findViewById(R.id.fab_delete);
+
 
         fab_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +90,8 @@ putRecycle();
                 intent.putExtra("note_describtion", "");
                 intent.putExtra("button", "save");
                 intent.putExtra("id", "");
-            startActivityForResult(intent,1);
+//                startActivity(intent);
+                startActivityForResult(intent, 1);
 
 
             }
@@ -60,15 +103,15 @@ putRecycle();
                     public void onClick(View v) {
                         NoteDataBase.getInstance(MainActivity.this).noteDao().deleteAllNOtes(list);
                         list.clear();
-
                         adapter.notifyDataSetChanged();
 
                     }
                 }
         );
     }
-    public void putRecycle(){
-         main_recycler = findViewById(R.id.main_recycler);
+
+    public void putRecycle() {
+        main_recycler = findViewById(R.id.main_recycler);
         list = NoteDataBase.getInstance(MainActivity.this).noteDao().getAllNotes();
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(MainActivity.this, 2);
         main_recycler.setLayoutManager(layoutManager);
@@ -83,14 +126,12 @@ putRecycle();
                 intent.putExtra("note_describtion", note.getDescription());
                 intent.putExtra("button", "update");
                 intent.putExtra("id", note.getId());
-                startActivityForResult(intent,1);
+//                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
 
             @Override
             public void imageOnClick(final int position) {
-
-                final int itemid=list.get(position).getId();
-
                 final AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
                         .setTitle("Delete")
                         .setMessage("Are you sure to delete this note?")
@@ -123,14 +164,14 @@ putRecycle();
             }
         });
         main_recycler.setAdapter(adapter);
-
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==1){
-            if(resultCode==RESULT_OK){
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
                 list = NoteDataBase.getInstance(MainActivity.this).noteDao().getAllNotes();
                 RecyclerView.LayoutManager layoutManager = new GridLayoutManager(MainActivity.this, 2);
                 main_recycler.setLayoutManager(layoutManager);
@@ -145,13 +186,14 @@ putRecycle();
                         intent.putExtra("note_describtion", note.getDescription());
                         intent.putExtra("button", "update");
                         intent.putExtra("id", note.getId());
-                        startActivityForResult(intent,1);
+                      startActivityForResult(intent,1);
+                     //   startActivity(intent);
                     }
 
                     @Override
                     public void imageOnClick(final int position) {
 
-                        final int itemid=list.get(position).getId();
+                        final int itemid = list.get(position).getId();
 //                note_entity=new Note_Entity(NoteDataBase.getInstance(MainActivity.this).noteDao().getTitleById(itemid),
 //                        NoteDataBase.getInstance(MainActivity.this).noteDao().getDescriptionById(itemid),
 //                        NoteDataBase.getInstance(MainActivity.this).noteDao().getDateById(itemid));
@@ -218,7 +260,6 @@ putRecycle();
                     }
                 });
                 main_recycler.setAdapter(adapter);
-
 
 
             }
